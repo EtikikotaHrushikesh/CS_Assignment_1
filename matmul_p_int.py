089fac0f80ec593dc819b920f4f1dc5648f79522
EtikikotaHrushikesh <69776196+EtikikotaHrushikesh@users.noreply.github.com>
2022-09-02 13:24:09 +0530

    Add files via upload

---
+++ matmul_p_int.py
@@ -0,0 +1,28 @@
+import random as r
+import time
+
+# get the start time
+p_start = time.process_time()
+n=int(input())
+A = [[(r.randint(0,100)) for i in range (n)]for _ in range(n)]
+ 
+# take a 3x4 matrix   
+B = [[r.randint(0,100) for i in range (n)]for _ in range(n)]
+result = [[r.randint(0,100) for i in range (n)]for _ in range(n)]
+ 
+meat_start=time.process_time()
+for i in range(n):
+ 
+    # iterating by column by B
+    for j in range(n):
+ 
+        # iterating by rows of B
+        for k in range(n):
+            result[i][j] += A[i][k] * B[k][j]
+meat_end=time.process_time()
+meat_time = meat_end - meat_start
+print("The Time taken by meat portion of program is:",meat_time)
+p_end=time.process_time()
+p_time = p_end - p_start
+print("The Time taken by whole program is:",p_time)
+
